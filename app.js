f010368c08146566f6962934eb41f9b97aee8c84
Steve Lee <imslee100@gmail.com>
2022-01-31 00:18:25 +0900

    Refactoring Router Class

--- app.js
+++ app.js
@@ -1,80 +1,17 @@
 const express = require('express')
+const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
-let users = require('./db/users')
-const app = express()
+const user = require('./api/user')
 
-function logger(req, res, next){
-    // console.log('i am logger')
-    next()
-}
-
-app.use(logger)
 app.use(morgan('dev'))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extend: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/users',user)
 
 app.listen(3000, function(){
-    // console.log('Server is running')
-})
-
-app.get('/users', function(req, res){
-    req.query.limit = req.query.limit || 10
-    const limit = parseInt(req.query.limit, 10)
-
-    if(Number.isNaN(limit)) {
-        return res.status(400).end()
-    }
-
-    res.json(users.slice(0,limit))
-})
-
-app.get('/users/:id', function(req, res){
-    const id = parseInt(req.params.id,10)
-    if(Number.isNaN(id)) return res.status(400).end()
-    
-    const user = users.filter((user) => user.id === id)[0]
-    if(!user) return res.status(404).end()
-    res.json(user)
-})
-
-app.delete('/users/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    if (Number.isNaN(id)) return res.status(400).end();
-  
-    users = users.filter(user=> user.id !== id);
-    res.status(204).end();
-});
-
-app.post('/users', (req,res) => {
-    const name = req.body.name
-    if(!name) return res.status(400).end()
-
-    const isConflict = users.filter(user => user.name === name).length
-    if(isConflict) return res.status(409).end()
-    
-    const id = Date.now()
-    const user = {id, name}
-    users.push(user)
-    res.status(201).json(user)
-})
-
-app.put('/users/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    if (Number.isNaN(id)) return res.status(400).end();
-  
-    const name = req.body.name;
-    if (!name) return res.status(400).end();
-  
-    const isConflict = users.filter(user => user.name === name).length;
-    if (isConflict) return res.status(409).end();
-  
-    const user = users.filter(user=> user.id === id)[0];
-    if (!user) return res.status(404).end();
-  
-    user.name = name;
-  
-    res.json(user);
+    console.log('App listening on port 3000!')
 })
 
 module.exports = app;
