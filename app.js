ec1d2c8403f79f5d53fed8897a84085880a1fb95
Steve Lee <imslee100@gmail.com>
2022-01-30 17:13:34 +0900

    TDD-User List Test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const users = require('./db/users')
 const app = express()
 
 function logger(req, res, next){
-    console.log('i am logger')
+    // console.log('i am logger')
     next()
 }
 
@@ -12,7 +12,7 @@ app.use(logger)
 app.use(morgan('dev'))
 
 app.listen(3000, function(){
-    console.log('Server is running')
+    // console.log('Server is running')
 })
 
 app.get('/users', function(req, res){
