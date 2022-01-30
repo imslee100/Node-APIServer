38f87be14e761df75d48d284dd94550f22dce058
Steve Lee <imslee100@gmail.com>
2022-01-30 21:22:47 +0900

    TDD-User Lookup

--- app.js
+++ app.js
@@ -16,7 +16,23 @@ app.listen(3000, function(){
 })
 
 app.get('/users', function(req, res){
-    res.json(users)
+    req.query.limit = req.query.limit || 10
+    const limit = parseInt(req.query.limit, 10)
+
+    if(Number.isNaN(limit)) {
+        return res.status(400).end()
+    }
+
+    res.json(users.slice(0,limit))
+})
+
+app.get('/users/:id', function(req, res){
+    const id = parseInt(req.params.id,10)
+    if(Number.isNaN(id)) return res.status(400).end()
+    
+    const user = users.filter((user) => user.id === id)[0]
+    if(!user) return res.status(404).end()
+    res.json(user)
 })
 
 module.exports = app;
--- tdd/supertest/app.spec.js
+++ tdd/supertest/app.spec.js
@@ -12,6 +12,52 @@ describe('GET /users는', () => {
                     done()
                 })
         })
+
+        it('최대 limit갯수만큼 응답한다.', (done) => {
+            request(app)
+                .get('/users?limit=2')
+                .end((err, res) => {
+                    res.body.should.have.lengthOf(2)
+                    done()
+                })
+        })
+    })
+
+    describe('실패시', () => {
+        it('limit이 숫자형이 아니면 400을 응답한다', (done) => {
+            request(app)
+                .get('/users?limit=two')
+                .expect(400)
+                .end(done);
+        })
+    })
+})
+
+describe('GET /users/1는', () => {
+    describe('성공시', () => {
+        it('id가 1인 유저 객체를 반환한다', (done) => {
+            request(app)
+                .get('/users/1')
+                .end((err,res) => {
+                    res.body.should.have.property('id',1)
+                    done()
+                })
+        })
+    })
+
+    describe('실패시', () => {
+        it('id가 숫자가 아닐경우 400으로 응답한다', (done) => {
+            request(app)
+                .get('/users/one')
+                .expect(400)
+                .end(done)
+        })
+        it('id가 유저를 찾을 수 없을 경우 404로 응답한다', (done) => {
+            request(app)
+                .get('/users/999')
+                .expect(404)
+                .end(done)
+        })
     })
 
 })
